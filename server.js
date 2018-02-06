b4de5af756930e2a88be394f5d6b09fc9446b902
Satyam patro <satyampatro077@gmail.com>
2018-02-06 20:10:15 +0530

    added new template

--- server.js
+++ server.js
@@ -9,16 +9,64 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/assets/stylesheet/main.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'assets', 'stylesheet', 'main.css'));
+app.get('/ui/dashboard.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dashboard.html'));
 });
 
-app.get('/ui/js/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'js', 'main.js'));
+
+app.get('/ui/form-register.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'form-register.html'));
 });
 
-app.get('/ui/assets/img/preview01.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'assets', 'img', 'preview01.jpg'));
+app.get('/ui/assets/css/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'css', 'style.css'));
+});
+
+app.get('/ui/assets/css/bootstrap.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'css', 'bootstrap.css'));
+});
+
+app.get('/ui/assets/css/ionicons.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'css', 'ionicons.css'));
+});
+
+app.get('/ui/assets/js/source/jquery.fancybox.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'source', 'jquery.fancybox.css'));
+});
+
+app.get('/ui/assets/css/form-register.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'css', 'form-register.css'));
+});
+
+
+app.get('/ui/assets/js/custom.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'custom.js'));
+});
+
+app.get('/ui/assets/js/jquery-1.11.1.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'jquery-1.11.1.js'));
+});
+
+
+app.get('/ui/assets/js/bootstrap.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'bootstrap.js'));
+});
+
+
+app.get('/ui/assets/js/source/jquery.fancybox.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'source','jquery.fancybox.js'));
+});
+
+app.get('/ui/assets/js/jquery.isotope.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'jquery.isotope.js'));
+});
+
+app.get('/ui/assets/js/scrollReveal.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'js', 'scrollReveal.js'));
+});
+
+app.get('/ui/assets/img/logo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'img', 'logo.png'));
 });
 
 var port = 8080; 
