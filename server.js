a7648f361ad95517d794cf2bf50db6976bc5c3dc
Satyam patro <satyampatro077@gmail.com>
2018-02-06 13:40:50 +0530

    added ui files

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/main.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+app.get('/ui/assets/stylesheet/main.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'stylesheet', 'main.css'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/js/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'js', 'main.js'));
 });
 
-app.get('/ui/preview01.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preview01.jpg'));
+app.get('/ui/assets/img/preview01.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'assets', 'img', 'preview01.jpg'));
 });
 
 var port = 8080; 
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`app listening on port ${port}!`);
 });
